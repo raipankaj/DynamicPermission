4b896967fb502daea6d632e2c3c681a69cb0c861
Pankaj Rai <pankaj.rai16@gmail.com>
2019-02-03 18:25:24 +0530

    Migrated to androidX

---
+++ app/src/main/java/lib/pankaj/dynamicpermission/MainActivity.kt
@@ -0,0 +1,12 @@
+package lib.pankaj.dynamicpermission
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
